54f67805d0ae738988a8741e054f4aa5ee6b630d
igor_matteus <igor_matteus@yahoo.com.br>
2011-11-15 22:09:50 +0000

    ADD FUNCOES LOGIN EM BANCO DE DADOS E ADD CLASSE Login.py FUNCIONANDO OK COM O BANCO

--- Resource/db.py
+++ Resource/db.py
@@ -7,6 +7,7 @@ class db:
         self.connection = MySQLdb.connect(host="db4free.net", db="academicsys", user="desenvolvedores", passwd="acesso")
         self.cursor = self.connection.cursor()
 
+    #***AREA DE COMUNICACAO COM A CLASSE DATA***
     def carregarData(self, ide):
         """Carrega uma data apardir da ID"""
         self.__id = str(ide)
@@ -42,3 +43,38 @@ class db:
                 return True
         except:
             return False
+
+    #***AREA DE COMUNICACAO COM A CLASSE ENDERECO***
+    def carregarLogin(self, loginId):
+        """Carrega uma usuario apardir da ID"""
+        self.__id = str(loginId)
+        self.cursor.execute("SELECT * FROM LOGIN WHERE USUARIO = %s;", (self.__id))
+        if self.cursor.rowcount == 1:
+            return self.cursor.fetchone()
+        else:
+            return None
+
+    def editarLogin(self, ide, usuario = None, senha = None, tipo = None):
+        """Salva um ajuste efetuado na data"""
+        try:
+            if ide is None:
+                return False
+            else:
+                if usuario != None:
+                    self.cursor.execute("UPDATE LOGIN SET USUARIO = '%s' WHERE ID = %d;" %(usuario, ide))
+                if senha != None:
+                    self.cursor.execute("UPDATE LOGIN SET SENHA = '%s' WHERE ID = %d;" %(senha, ide))
+                if tipo != None:
+                    self.cursor.execute("UPDATE LOGIN SET TIPO = '%s' WHERE ID = %d;" %(tipo, ide))
+                return True
+        except:
+            return False
+
+    def validarLogin(self, usuario, senha):
+        """Retorna bool para uma validacao"""
+        if usuario is not None and senha is not None:
+            cursor.execute("SELECT SENHA FROM LOGIN WHERE USUARIO = %s;" %(usuario))
+            self.__temp = cursor.fetchone()
+            if self.__temp is not None and self.__temp[0] is senha:
+                return True
+        return False
