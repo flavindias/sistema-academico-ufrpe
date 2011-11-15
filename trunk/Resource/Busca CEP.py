0d0af996c74e6b8b471966f012fb040f44882951
diasflaviano@gmail.com <diasflaviano@gmail.com@640c1a12-a844-cf17-f425-50fdd63615f4>
2011-11-15 13:55:07 +0000

    Buscador de CEP ONLINE

---
+++ trunk/Resource/Busca CEP.py
@@ -0,0 +1,27 @@
+#! /usr/bin/env python  
+# -*- coding: iso-8859-1 -*-    
+import urllib  
+import cgi  
+  
+#  
+#   Busca CEP  
+#  
+cep_busca   = raw_input("Digite os numeros do seu CEP: ");  
+url         = "http://cep.republicavirtual.com.br/web_cep.php?cep=" + cep_busca + "&formato=query_string"  
+pagina      = urllib.urlopen(url)  
+conteudo    = pagina.read();  
+resultado   = cgi.parse_qs(conteudo);  
+  
+if resultado['resultado'][0] == '1':  
+    print "Endereço com cidade de CEP único: "  
+    print resultado['tipo_logradouro'][0]  
+    print resultado['logradouro'][0]  
+    print resultado['bairro'][0]  
+    print resultado['cidade'][0]  
+    print resultado['uf'][0]  
+elif resultado['resultado'][0] == '2':  
+    print "Endereço com cidade de CEP único: "  
+    print resultado['cidade'][0]  
+    print resultado['uf'][0]  
+else:  
+    print "CEP não encontrado"  
