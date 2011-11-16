f7ee111dc34b191ccfe2e5b572524ac7ab9fadaf
igor_matteus <igor_matteus@yahoo.com.br>
2011-11-16 03:30:59 +0000

    Add DadosPessoais.py

--- Resource/db.py
+++ Resource/db.py
@@ -24,9 +24,9 @@ class db:
                 return False
             else:
                 self.__temp = str(ano)+'-'+str(mes)+'-'+str(dia)
-                self.cursor.execute("UPDATE DATAS SET DAT = '%s' WHERE ID = %d;" %(self.__temp, ide))
+                self.cursor.execute("UPDATE DATAS SET DAT = '%s' WHERE ID = %s;" %(self.__temp, ide))
                 if hora == None:
-                    self.cursor.execute("UPDATE DATAS SET HORA = '%s' WHERE ID = %d;" %(hora, ide))
+                    self.cursor.execute("UPDATE DATAS SET HORA = '%s' WHERE ID = %s;" %(hora, ide))
                 return True
         except:
             return False
@@ -61,11 +61,11 @@ class db:
                 return False
             else:
                 if usuario != None:
-                    self.cursor.execute("UPDATE LOGIN SET USUARIO = '%s' WHERE ID = %d;" %(usuario, ide))
+                    self.cursor.execute("UPDATE LOGIN SET USUARIO = '%s' WHERE ID = %s;" %(usuario, ide))
                 if senha != None:
-                    self.cursor.execute("UPDATE LOGIN SET SENHA = '%s' WHERE ID = %d;" %(senha, ide))
+                    self.cursor.execute("UPDATE LOGIN SET SENHA = '%s' WHERE ID = %s;" %(senha, ide))
                 if tipo != None:
-                    self.cursor.execute("UPDATE LOGIN SET TIPO = '%s' WHERE ID = %d;" %(tipo, ide))
+                    self.cursor.execute("UPDATE LOGIN SET TIPO = '%s' WHERE ID = %s;" %(tipo, ide))
                 return True
         except:
             return False
@@ -109,20 +109,20 @@ class db:
         except:
             return False
 
-    def editarEndereco(self, ide, rua, num, bairro, cidade, uf, cep, complemento):
+    def editarEndereco(self, ide, rua = None, num = None, bairro = None, cidade = None, uf = None, cep = None, complemento = None):
         """Salva um ajuste efetuado na endereco"""
         try:
             if ide is None:
                 return False
             else:
                 if rua != None:
-                    self.cursor.execute("UPDATE ENDERECO SET RUA = '%s' WHERE ID = %d;" %(rua, ide))
+                    self.cursor.execute("UPDATE ENDERECO SET RUA = '%s' WHERE ID = %s;" %(rua, ide))
                 if num != None:
-                    self.cursor.execute("UPDATE ENDERECO SET NUM = %s WHERE ID = %d;" %(num, ide))
+                    self.cursor.execute("UPDATE ENDERECO SET NUM = %s WHERE ID = %s;" %(num, ide))
                 if bairro != None:
-                    self.cursor.execute("UPDATE ENDERECO SET BAIRRO = '%s' WHERE ID = %d;" %(bairro, ide))
+                    self.cursor.execute("UPDATE ENDERECO SET BAIRRO = '%s' WHERE ID = %s;" %(bairro, ide))
                 if cidade != None:
-                    self.cursor.execute("UPDATE ENDERECO SET CIDADE = '%s' WHERE ID = %d;" %(cidade, ide))
+                    self.cursor.execute("UPDATE ENDERECO SET CIDADE = '%s' WHERE ID = %s;" %(cidade, ide))
                 if uf != None:
                     self.cursor.execute("UPDATE ENDERECO SET UF = '%s' WHERE ID = %s;" %(uf, ide))
                 if cep != None:
@@ -167,46 +167,52 @@ class db:
 
     #***AREA DE COMUNICACAO COM A CLASSE DADOSPESSOAIS***
     def carregarDadosPessoais(self, documento):
-        """Carrega uma DadoPessoai a partir da ID"""
+        """Carrega uma DadoPessoais a partir do documento"""
         self.__id = int(documento)
-        self.cursor.execute("SELECT * FROM DADOS_PESSOAIS WHERE ID = %s;", (self.__id))
+        self.cursor.execute("SELECT * FROM DADOS_PESSOAIS WHERE DOCUMENTO = '%s';", (self.__id))
         if self.cursor.rowcount == 1:
             return self.cursor.fetchone()
         else:
             return None
 
-    def addDadosPessoais(self, documento, nome, sexo, celular):
+    def addDadosPessoais(self, documento, nome, sexo, dataNascId, enderecoId, celular = None, fixo = None):
         """Cria um novo endereco no banco de dados"""
+        if sexo:
+            sexo = 'TRUE'
+        else:
+            sexo = 'FALSE'
         try:
-            if complemento is None:
-                self.cursor.execute("INSERT INTO ENDERECO(ID, RUA, NUM, BAIRRO, CIDADE, UF, CEP, COMPLEMENTO) VALUES (NULL, '%s', %s, '%s', '%s', '%s', '%s', NULL);" %(rua, num, bairro, cidade, uf, cep))
-                return True
-            else:
-                self.cursor.execute("INSERT INTO ENDERECO(ID, RUA, NUM, BAIRRO, CIDADE, UF, CEP, COMPLEMENTO) VALUES (NULL, '%s', %s, '%s', '%s', '%s', '%s', '%s');" %(rua, num, bairro, cidade, uf, cep, complemento))
-                return True
+            self.cursor.execute("INSERT INTO DADOS_PESSOAIS(DOCUMENTO, NOME, SEXO, CELULAR, FIXO, DATA_NASC, ENDERECO) VALUES ('%s', '%s', %s, NULL, NULL, %s, %s);" %(documento, nome, sexo, dataNascId, enderecoId))
+            if celular is not None:
+                self.cursor.execute("UPDATE DADOS_PESSOAIS SET CELULAR = '%s' WHERE DOCUMENTO = '%s';" %(celular, documento))
+            if fixo is not None:
+                self.cursor.execute("UPDATE DADOS_PESSOAIS SET FIXO = '%s' WHERE DOCUMENTO = '%s';" %(fixo, documento))
+            return True
         except:
             return False
 
-    def editarDadosPessoais(self, ide, rua, num, bairro, cidade, uf, cep, complemento):
+    def editarDadosPessoais(self, documento = None, nome = None, sexo = None, dataNascId = None, enderecoId = None, celular = None, fixo = None):
         """Salva um ajuste efetuado na endereco"""
         try:
-            if ide is None:
+            if documento is None:
                 return False
             else:
-                if rua != None:
-                    self.cursor.execute("UPDATE ENDERECO SET RUA = '%s' WHERE ID = %d;" %(rua, ide))
-                if num != None:
-                    self.cursor.execute("UPDATE ENDERECO SET NUM = %s WHERE ID = %d;" %(num, ide))
-                if bairro != None:
-                    self.cursor.execute("UPDATE ENDERECO SET BAIRRO = '%s' WHERE ID = %d;" %(bairro, ide))
-                if cidade != None:
-                    self.cursor.execute("UPDATE ENDERECO SET CIDADE = '%s' WHERE ID = %d;" %(cidade, ide))
-                if uf != None:
-                    self.cursor.execute("UPDATE ENDERECO SET UF = '%s' WHERE ID = %s;" %(uf, ide))
-                if cep != None:
-                    self.cursor.execute("UPDATE ENDERECO SET CEP = '%s' WHERE ID = %s;" %(cep, ide))
-                if complemento != None:
-                    self.cursor.execute("UPDATE ENDERECO SET COMPLEMENTO = '%s' WHERE ID = %s;" %(complemento, ide))
+                if nome != None:
+                    self.cursor.execute("UPDATE DADOS_PESSOAIS SET NOME = '%s' WHERE DOCUMENTO = '%s';" %(rua, ide))
+                if sexo != None:
+                    if sexo:
+                        sexo = 'TRUE'
+                    else:
+                        sexo = 'FALSE'
+                    self.cursor.execute("UPDATE DADOS_PESSOAIS SET SEXO = %s WHERE DOCUMENTO = '%s';" %(num, ide))
+                if dataNascId != None:
+                    self.cursor.execute("UPDATE DADOS_PESSOAIS SET DATA_NASC = %s WHERE DOCUMENTO = '%s';" %(bairro, ide))
+                if enderecoId != None:
+                    self.cursor.execute("UPDATE DADOS_PESSOAIS SET ENDERECO = %s WHERE DOCUMENTO = '%s';" %(cidade, ide))
+                if celular != None:
+                    self.cursor.execute("UPDATE DADOS_PESSOAIS SET CELULAR = '%s' WHERE DOCUMENTO = '%s';" %(uf, ide))
+                if fixo != None:
+                    self.cursor.execute("UPDATE DADOS_PESSOAIS SET FIXO = '%s' WHERE DOCUMENTO = '%s';" %(cep, ide))
                 return True
         except:
             return False
