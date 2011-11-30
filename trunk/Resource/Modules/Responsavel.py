52951f334e1b921d349fd164b46ac2e248a391ac
igor_matteus@yahoo.com.br <igor_matteus@yahoo.com.br@640c1a12-a844-cf17-f425-50fdd63615f4>
2011-11-30 03:13:49 +0000

    att em Resource/Interface Gráfica/FrameGerAluno.py att em Resource/Modules/db.py nas funçoes de Responsavel att em Resource/Modules/Responsavel.py comunicando com o Resource/Modules/db.py ajuste em Resource/Modules/Professor.py

--- trunk/Resource/Modules/Responsavel.py
+++ trunk/Resource/Modules/Responsavel.py
@@ -1,40 +1,67 @@
 #!/usr/bin/env python
 #-*-coding: latin1-*-
-'''
+"""
 Created on 15/11/2011
 
 @author: Grupo Sistema Academico
-'''
+"""
 class Responsavel:
-    '''Classe para Gerenciamento de Conta do Responsavel'''
-    def __init__(self):
-        self.__id = None # INT
-        self.__dadosId = None # STR
-        self.__loginId = None # STR
-    '''Retorna Id'''
+    """Classe para Gerenciamento de Conta do Responsavel"""
+    def __init__(self, ide):
+        """Retorna Id do Professor"""
+        self.__DataBase = db()
+        if ide is None:
+            self.__id = None # INT
+            self.__dadosId = None # STR
+            self.__loginId = None # STR
+        else:
+            self.carregar(ide)
+            self.setId(ide)
+
     def getId(self):
+        """Retorna Id"""
         return self.__id
-    '''Edita Id'''
     def setId(self, novoId):
+        """Edita Id"""
         self.__id = novoId
-        return 0
-    '''Retorna dadosId'''
+
     def getDadosId(self):
+        """Retorna dadosId"""
         return self.__dadosId
-    '''Edita dadosId'''
     def setDadosId(self, novoDadosId):
+        """Edita dadosId"""
         self.__dadosId = novoDadosId
-        return 0
-    '''Retorna Id do Login'''
+
     def getLoginId(self):
+        """Retorna Id do Login"""
         return self.__loginId
-    '''Edita Id do Login'''
     def setLoginId(self, novoLoginId):
+        """Edita Id do Login"""
         self.__loginId = novoLoginId
-        return 0
-    '''Salva alterações no banco de dados'''
-    def salvarEdit(self, id):
-        return True #Retorna True ou False
-    '''Carrega as informações de acordo com o id informado atraves do banco de dados'''
-    def carregar(self, id):
-        return True #Retorna True ou False
+
+    def carregar(self, ide):
+        """Carrega responsavel a partir de id no BD"""
+        self.__dados = self.__DataBase.carregarResponsavel(ide)
+        if self.__dados == None:
+            return False
+        else:
+            self.setId(self.__dados[0])
+            self.setDadosId(self.__dados[1])
+            self.setLoginId(self.__dados[2])
+            return True
+
+    def salvarEdit(self, ide):
+        """Salva ajuste de responsavel no BD"""
+        return self.__DataBase.editarResponsavel(ide, self.getDadosId(), self.getLoginId())
+
+    def addNovo(self):
+        """Cria um novo responsavel no BD"""
+        return self.__DataBase.addResponsavel(self.getDadosId(), self.getLoginId())
+
+    def delete(self, ide):
+        """Deleta um responsavel a partir da ID"""
+        return self.__DataBase.delResponsavel(ide)
+
+    def pegarId(self):
+        """Retorna uma lista com todas as IDs de responsaveis"""
+        return self.__DataBase.returnIdResponsavel()
